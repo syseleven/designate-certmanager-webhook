25d02862b101e15c8bf56fdeb8528e4cfe9d076a
Olaf Klischat <olaf.klischat@gmail.com>
2026-06-05 09:11:53 +0200

    Integration test cleaned up
    * get rid of New() -- was only called from our test setup, and
      Initialize() does the same thing and is also called by the test setup
    
    * get rid of empty testdata config file -- it'll always be empty, just
      set it in code
    
    Signed-off-by: Olaf Klischat <olaf.klischat@gmail.com>

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ import (
 	log "github.com/sirupsen/logrus"
 	"k8s.io/client-go/rest"
 
-	"github.com/cert-manager/cert-manager/pkg/acme/webhook"
 	"github.com/cert-manager/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
 	"github.com/cert-manager/cert-manager/pkg/acme/webhook/cmd"
 )
@@ -27,16 +26,6 @@ func main() {
 
 type designateDNSProviderSolver struct {
 	client *gophercloud.ServiceClient
-}
-
-func New() webhook.Solver {
-	client, err := createDesignateServiceClient()
-	if err != nil {
-		log.Panic(fmt.Errorf("failed to create designate client: %w", err))
-	}
-	return &designateDNSProviderSolver{
-		client: client,
-	}
 }
 
 func (c *designateDNSProviderSolver) Name() string {
--- main_test.go
+++ main_test.go
@@ -7,20 +7,18 @@ import (
 	acmetest "github.com/cert-manager/cert-manager/test/acme"
 )
 
-var (
-	zone = os.Getenv("TEST_ZONE_NAME")
-)
-
 func TestRunsSuite(t *testing.T) {
-	// The manifest path should contain a file named config.json that is a
-	// snippet of valid configuration that should be included on the
-	// ChallengeRequest passed as part of the test cases.
+	zone := os.Getenv("TEST_ZONE_NAME")
+	if zone == "" {
+		t.Fatalf("TEST_ZONE_NAME is not set")
+	}
 
-	solver := New()
+	solver := &designateDNSProviderSolver{}
 	fixture := acmetest.NewFixture(solver,
 		acmetest.SetResolvedZone(zone),
+		acmetest.SetDNSName("*."+zone),
 		acmetest.SetAllowAmbientCredentials(false),
-		acmetest.SetManifestPath("testdata/my-custom-solver"),
+		acmetest.SetConfig("{}"),
 	)
 
 	fixture.RunBasic(t)
